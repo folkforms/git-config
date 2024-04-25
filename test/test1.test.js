af677389902701fc3cf6468468fd357c5377ee2f
folkforms <alan.bergin@gmail.com>
2024-04-25 21:59:26 +0100

    Add more specific test

--- test/test1.test.js
+++ test/test1.test.js
@@ -5,6 +5,14 @@ test("it matches repo patterns", () => {
   const gitUtils = dummyGitUtils({ remoteUrl: "company-internal/foo.git" });
   const testModule = (settings) => {
     expect(settings.length).toEqual(6);
+    expect(settings).toEqual([
+      "alias.amend=commit --amend --no-edit",
+      "alias.cb=checkout -b",
+      "user.name=MyCompanyUserName",
+      "user.email=mycompanyemail@company.com",
+      "user.signingkey=SomeSigningKey",
+      "commit.gpgsign=true",
+    ]);
   };
 
   gitConfig(
