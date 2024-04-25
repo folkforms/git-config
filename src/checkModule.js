63a71dad0c3f331137c8d2ff963a31ecaf586628
folkforms <alan.bergin@gmail.com>
2024-04-25 22:30:47 +0100

    It checks config and prints incorrect values

--- src/checkModule.js
+++ src/checkModule.js
@@ -1,8 +1,32 @@
 const checkModule = (configToApply, gitUtils, shell, dryRun) => {
-  // Call shell with each configToApply
-  configToApply.forEach((item) => {
-    shell.echo(item);
+  const globalConfig = convertToObjects(gitUtils.getGlobalConfig());
+  const localConfig = convertToObjects(gitUtils.getLocalConfig());
+  console.log(`globalConfig = ${JSON.stringify(globalConfig)}`);
+  console.log(`localConfig = ${JSON.stringify(localConfig)}`);
+  const mergedConfig = { ...globalConfig, ...localConfig };
+  console.log(`mergedConfig = ${JSON.stringify(mergedConfig)}`);
+
+  const configToApplyAsObjects = convertToObjects(configToApply);
+  console.log(
+    `configToApplyAsObjects = ${JSON.stringify(configToApplyAsObjects)}`,
+  );
+
+  Object.keys(configToApplyAsObjects).forEach((key) => {
+    if (mergedConfig[key] !== configToApplyAsObjects[key]) {
+      shell.echo(`git config ${key} ${configToApplyAsObjects[key]}`);
+    }
   });
+};
+
+const convertToObjects = (arr) => {
+  const out = {};
+  arr.forEach((item) => {
+    const equalsIndex = item.indexOf("=");
+    const key = item.substring(0, equalsIndex);
+    const value = item.substring(equalsIndex + 1);
+    out[key] = value;
+  });
+  return out;
 };
 
 module.exports = checkModule;
--- test/checkModule.test.js
+++ test/checkModule.test.js
@@ -2,10 +2,39 @@ const { dummyShellJs } = require("dummy-shells");
 const dummyGitUtils = require("./dummyGitUtils");
 const checkModule = require("../src/checkModule");
 
-test("it calls shell with the input commands", () => {
+test("it does nothing when settings are already correct globally and local setting is not set", () => {
   dummyShellJs._clear();
-  const gitUtils = dummyGitUtils({ remoteUrl: "company-internal/foo.git" });
-  const settings = ["xxx", "yyy"];
-  checkModule(settings, gitUtils, dummyShellJs);
-  expect(dummyShellJs.echoList).toEqual(settings);
+  const gitUtils = dummyGitUtils({
+    globalConfig: ["user.name=CorrectUsername"],
+    localConfig: [],
+  });
+  const configToApply = ["user.name=CorrectUsername"];
+  checkModule(configToApply, gitUtils, dummyShellJs);
+  expect(dummyShellJs.echoList).toEqual([]);
+});
+
+test("it does nothing when settings are already correct locally", () => {
+  dummyShellJs._clear();
+  const gitUtils = dummyGitUtils({
+    globalConfig: [
+      "user.name=IncorrectUsernameButItWillBeIgnoredDueToCorrectLocalUsername",
+    ],
+    localConfig: ["user.name=CorrectUsername"],
+  });
+  const configToApply = ["user.name=CorrectUsername"];
+  checkModule(configToApply, gitUtils, dummyShellJs);
+  expect(dummyShellJs.echoList).toEqual([]);
+});
+
+test("it prints an error when settings are not correct", () => {
+  dummyShellJs._clear();
+  const gitUtils = dummyGitUtils({
+    globalConfig: ["user.name=ArbitraryUserName"],
+    localConfig: ["user.name=WrongUsername"],
+  });
+  const configToApply = ["user.name=MyCompanyUserName"];
+  checkModule(configToApply, gitUtils, dummyShellJs);
+  expect(dummyShellJs.echoList).toEqual([
+    "git config user.name MyCompanyUserName",
+  ]);
 });
